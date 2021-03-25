b5260fbb5a298c5feba007d25c04c1755ede6949
Murdo Maclachlan <murdo@maclachlans.org.uk>
2021-03-25 14:21:23 +0000

    Create materials.lua

---
+++ import_skeletonWarrior/scripts/materials.lua
@@ -0,0 +1,27 @@
+defineMaterial{
+    name = "skeleton_warrior",
+    diffuseMap = "assets/textures/monsters/skeleton_warrior_dif.tga",
+    specularMap = "assets/textures/monsters/skeleton_warrior_spec.tga",
+    normalMap = "assets/textures/monsters/skeleton_warrior_normal.tga",
+    doubleSided = false,
+    lighting = true,
+    alphaTest = false,
+    blendMode = "Opaque",
+    textureAddressMode = "Wrap",
+    glossiness = 75,
+    depthBias = 0,
+}
+
+defineMaterial{
+    name = "skeleton_warrior_weapons",
+    diffuseMap = "assets/textures/monsters/skeleton_warrior_weapons_dif.tga",
+    specularMap = "assets/textures/monsters/skeleton_warrior_weapons_spec.tga",
+    normalMap = "assets/textures/monsters/skeleton_warrior_weapons_normal.tga",
+    doubleSided = false,
+    lighting = true,
+    alphaTest = false,
+    blendMode = "Opaque",
+    textureAddressMode = "Wrap",
+    glossiness = 50,
+    depthBias = 0,
+}
