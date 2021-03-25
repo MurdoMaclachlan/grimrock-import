a5bc9deb3f4ef36e2671e05e6b3a42170db0eb91
Murdo Maclachlan <murdo@maclachlans.org.uk>
2021-03-25 14:27:44 +0000

    Create init.lua

---
+++ import_skeletonWarrior_WS/init.lua
@@ -0,0 +1,4 @@
+import "mod_assets/import_skeletonWarrior/scripts/items.lua"
+import "mod_assets/import_skeletonWarrior/scripts/materials.lua"
+import "mod_assets/import_skeletonWarrior/scripts/monsters.lua"
+import "mod_assets/import_skeletonWarrior/scripts/monsters_ws.lua"
